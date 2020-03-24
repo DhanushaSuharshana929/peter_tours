6bdc78c68c7d006e80e8659a282689a772ec57dc
uditha1998 <uditha.synotec@gmail.com>
2020-03-24 15:43:17 +0530

    #43 Tuk Tuk Tours

---
+++ view-tuk-tours.php
@@ -0,0 +1,137 @@
+<?php
+include './class/include.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+
+    <head>
+        <!-- Required meta tags -->
+         <meta charset="utf-8">
+        
+        <meta name="description" content="We offer populatt Tuk Tuk Ride taxi service to our customers and let them to experience the real chill of this enjoyable source transportation that can be seen in Sri Lanka." />
+        <meta name="keywords" content=".tuk tuk ride srilanka, taxi service sri lanka, thereewheeler ride in sri lanaka, tuk tuk tours, best tour packages in sri lanka, , things to do in sri lanka,  galle tours ,srilanka peter tours" />
+        <meta name="author" content="Synotec Holdings Private Limited" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">       <title>Peter Tours</title>
+ <title>Peter Tours</title>
+
+
+        <!------------------------------------------
+        Main CSS File
+      <------------------------------------------>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="css/menu.css">
+        <link rel="stylesheet" href="css/custom-animation.css">
+        <!-- Style CSS -->
+        <link rel="stylesheet" href="css/style.css">
+        <!-- Responsive CSS -->
+        <link rel="stylesheet" href="css/responsive.css">
+
+        <!-- Favicon -->
+        <link href="images/favicon.png" rel="shortcut icon" type="image/png">
+
+    </head>
+
+    <body>
+
+
+        <!-- Start Page Wrapper  -->
+        <div class="page-wrapper">
+
+            <!-- Header Section Start -->
+            <?php
+            include("./innerheader.php");
+            ?>
+            <!-- Header Section End -->
+            <!-- Inner Section Start -->
+            <section class="inner-area banner-parralax">
+                <div class="container">
+                    <div class="section-content">
+                        <div class="row">
+                            <div class="col-12">
+                                <h4>TUK TUK TOURS</h4>
+
+                                <p><a href="index.php">Home</a> -> TUK TUK TOURS</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Inner Section End -->
+
+            <!-- Blog Section Start -->
+            <section class="blog-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 ">
+                            <div class="blog-details">
+
+                                <div class="view_carousel owl-carousel owl-theme owl-navst">
+                                        <div class="thumb">
+                                            <img src="images/tuktuk.jpg" alt="">
+                                        </div>
+                                        <div class="thumb">
+                                            <img src="images/tuktuk2.jpg" alt="">
+                                        </div>
+                                                                       
+
+
+                                </div>
+                                <br>
+                                <div class="details-content mb-40">
+
+                                    <p class="mb-20">Threewheeler is  a major source transportation in Sri Lanka which is known as "Tuk Tuk" beacause of it sound.This Amazing vehicle is so easy to drive and anyone can  learn to drive this thing too much easily . You can have great view of the streets and the surrounding nature during a Tuk Tuk Ride.Tuk Tuk ride will give you too much fun with the delightful breeze from the boths sides of the tuk tuk and it is the best way to enjoy the travel just like a real Sri Lankan.Tuk Ride is one of the most economical way to travel in Sri Lanka Just after the frustrated bus ride.Tuk Tuk Ride is also famous because of it sound system which installed at just behind the Backsheet.</p>
+
+
+                                </div>
+
+                            </div>
+                        </div>
+                    
+                    </div>
+                </div>
+            </section>
+            <!-- Blog Section End -->
+
+            <!-- Footer Style Seven Start -->
+            <?php
+            include("./footer.php");
+            ?>
+            <!-- Footer Style Seven End -->
+
+        </div>
+        <!-- End Page Wrapper  -->
+
+        <a href="#" class="scrollup"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
+
+
+        <!------------------------------------------
+        Main JavaScript
+      <------------------------------------------>
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+
+        <!-- Optional JavaScript -->
+        <script src="js/bootstrap-dropdownhover.min.js"></script>
+        <script src="js/bootstrap-slider.js"></script>
+        <script src="js/jquery.flexslider-min.js"></script>
+        <script src="js/slick.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/css3-animate-it.js"></script>
+        <script src="js/magnific-popup.min.js"></script>
+        <script src="js/jquery.fancybox.js"></script>
+        <script src="js/player-min.js"></script>
+
+        <!-- Custom JavaScript -->
+        <script src="js/script.js"></script>
+
+    </body>
+
+
+</html> 
